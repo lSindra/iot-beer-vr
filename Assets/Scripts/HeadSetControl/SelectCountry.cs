578fb253be06d6d3e8d08801aa9ae260582ca6bb
Lucas Sindra <lucas.sindra@sap.com>
2018-09-24 16:01:58 -0300

    Added sound effect and loading for countries

---
+++ Assets/Scripts/HeadSetControl/SelectCountry.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectCountry : MonoBehaviour
+{
+
+    public Toggle toggle;
+    public AudioSource audioEffect;
+    public string countryName;
+
+    void Start()
+    {
+        toggle.onValueChanged.AddListener(ToggleEffect);
+    }
+
+    void ToggleEffect(bool isOn)
+    {
+        audioEffect.PlayOneShot(audioEffect.clip);
+        SceneManager.LoadSceneAsync(countryName);
+    }
+}
