d46be20646ff60fb0dd11a48094934006851fd9e
Lucas Sindra <lucas.sindra@sap.com>
2018-10-29 13:23:17 -0300

    Added script for highlighting country

--- Assets/Scripts/Utils/CopyToggleColorForShader.cs
+++ Assets/Scripts/Utils/CopyToggleColorForShader.cs
@@ -2,32 +2,38 @@
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class CopyToggleColor : MonoBehaviour {
+public class CopyToggleColorForShader : MonoBehaviour {
 
-    public Graphic child;
+    public Material defaultMaterial;
     public Toggle toggle;
+    public GameObject thisObject;
+
+    Material material;
 
 	void Start () {
+        material = new Material(defaultMaterial);
+        thisObject.GetComponent<MeshRenderer>().material = material;
+
         EventTrigger eventTrigger = toggle.gameObject.AddComponent<EventTrigger>();
 
         EventTrigger.Entry enterEntry = new EventTrigger.Entry();
         enterEntry.eventID = EventTriggerType.PointerEnter;
-        enterEntry.callback.AddListener((data) => { EnterColor((PointerEventData)data); });
+        enterEntry.callback.AddListener((data) => { EnterColor(); });
 
         EventTrigger.Entry exitEntry = new EventTrigger.Entry();
         exitEntry.eventID = EventTriggerType.PointerExit;
-        exitEntry.callback.AddListener((data) => { ExitColor((PointerEventData)data); });
+        exitEntry.callback.AddListener((data) => { ExitColor(); });
 
         eventTrigger.triggers.Add(enterEntry);
         eventTrigger.triggers.Add(exitEntry);
     }
 
-    protected void EnterColor(PointerEventData data) {
-        child.color = toggle.colors.highlightedColor;
+    protected void EnterColor() {
+        material.color = toggle.colors.highlightedColor;
     }
 
-    protected void ExitColor(PointerEventData data)
+    protected void ExitColor()
     {
-        child.color = toggle.colors.normalColor;
+        material.color = defaultMaterial.color;
     }
 }
